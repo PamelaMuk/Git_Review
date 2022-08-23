cbd47ea8629a599cab774e7d6a0dcdddb907e7e6
PamelaMuk <ulupiquita@gmail.com>
2022-08-23 11:31:29 -0400

    adding the Bye world

---
+++ src/First/ByeWorld.java
@@ -0,0 +1,9 @@
+package First;
+
+public class ByeWorld {
+    public static void main(String[] args) {
+        System.out.println("Maybe 5 weeks java left");
+        System.out.println("1.5 weeks of git");
+
+    }
+}
